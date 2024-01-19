d0eb849acee41ae9540b9ebc349f4c0e97666a7e
VictoriaKonuhova <konyukhova.victoria@gmail.com>
2024-01-19 14:47:30 +0300

    timer

--- js/main.js
+++ js/main.js
@@ -1,32 +1,69 @@
 window.addEventListener('DOMContentLoaded', () => {
+  //tabs
   const tabs = document.querySelectorAll('.tabheader__item');
   const tabsContent = document.querySelectorAll('.tabcontent');
   const tabHeader = document.querySelector('.tabheader');
 
-  function hideTabContent(){
-    tabsContent.forEach(item=>{
+  function hideTabContent() {
+    tabsContent.forEach(item => {
       item.style.display = 'none';
     });
-    tabs.forEach(item=>{
+    tabs.forEach(item => {
       item.classList.remove('tabheader__item_active');
     });
   }
-  function showTabContent(i = 0){
+  function showTabContent(i = 0) {
     tabsContent[i].style.display = 'block';
     tabs[i].classList.add('tabheader__item_active');
   }
   hideTabContent();
   showTabContent();
 
-  tabHeader.addEventListener('click', (event)=>{
+  tabHeader.addEventListener('click', (event) => {
     const target = event.target;
-    if (target && target.classList.contains('tabheader__item')){
-      tabs.forEach((item, i)=>{
-        if(target == item){
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
           hideTabContent();
           showTabContent(i);
         }
       });
     }
   });
+  //timer
+  const timeEnd = '2024-04-28';
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    const days = Math.floor(t / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(t / (1000 * 60 * 60) % 24);
+    const minutes = Math.floor((t / 1000 / 60) % 60);
+    const seconds = Math.floor((t / 1000) % 60);
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = document.querySelector('#days');
+    const hours = document.querySelector('#hours');
+    const minutes = document.querySelector('#minutes');
+    const seconds = document.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = t.days;
+      hours.innerHTML = t.hours;
+      minutes.innerHTML = t.minutes;
+      seconds.innerHTML = t.seconds;
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+  setClock('.timer', timeEnd);
 });
